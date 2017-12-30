b4b2e13c38a45b5fe9c1d7124675815dc984f96a
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-30 16:27:49 +0000

    Add createController function to Boostrap with built in error messages

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -17,4 +17,25 @@ class Bootstrap{
       $this->action = $this->request['action'];
     }
   }
+
+// Need to refactor createController method
+  public function createController() {
+    if(class_exists($this->controller)){
+      $parents = class_parents($this->controller);
+      if(in_array("Controller", $parents)){
+        if(method_exists($this->controller, $this->action)){
+          return new $this->controller($this->action, $this->request);
+        } else {
+          echo '<h1>Method does not exist</h1>';
+          return;
+        }
+      } else {
+        echo '<h1>Base controller not found</h1>';
+        return;
+      }
+    } else {
+      echo '<h1>Controller class does not exist</h1>';
+      return;
+    }
+  }
 }
